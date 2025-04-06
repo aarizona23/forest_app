ecec47a5df256605f957f0cb3a6779beb72fffa8
momysheva <aliza.momysheva@nu.edu.kz>
2025-04-06 17:11:04 +0500

    updated url

--- forest_app/urls.py
+++ forest_app/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api_forest.urls import urlpatterns as api_forest_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forest/', include(api_forest_urls)),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
